ecee543f71cb1e37333421f00ce439e16254717a
jamesdcheney0 <106191522+jamesdcheney0@users.noreply.github.com>
2023-01-31 09:14:33 -0700

    Add subnet_id field to aws_instance
    When running this for real, it errored, unable to combine SG and instance. I deleted the .git files on my local cause they were being a pain

--- main.tf
+++ main.tf
@@ -74,11 +74,16 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
+data "aws_subnet_ids" "subnet_ids" {
+  vpc_id = data.aws_vpc.main.id
+}
+
 resource "aws_instance" "my_server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.sg_my_server.id]
+  subnet_id              = data.aws_subnet_ids.subnet_ids.ids[0]
   user_data              = data.template_file.user_data.rendered
 
   tags = {
